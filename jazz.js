265d64a3e47b376816b7002b171e039f8acae0d4
Dallas Kelle <dkelle@wireless-10-147-101-126.public.utexas.edu>
2016-03-02 16:57:49 -0600

    Now have CSS boxes

--- jazz.js
+++ jazz.js
@@ -1,35 +1,41 @@
-var songarr = ["three_to_get_ready", "walking_shoes", "out_of_nowhere", "night_at_the_turntable", "but_not_for_me", "all_the_things_you_are"];
 
-//Populate the array of songs, and pick one of them randomly
+//populate the array of song titles
+var songarr = ["three_to_get_ready", "walking_shoes", "out_of_nowhere", "night_at_the_turntable", "but_not_for_me", "all_the_things_you_are"];
 var songnum = Math.floor(Math.random()*songarr.length);
 var songname = songarr[songnum];
+
 //Create an array full of random titles
-var rantitle = new Array();
+var answerChoices = new Array();
 for(var i = 0; i < 4; i ++)
 {
-	var ran = Math.floor(Math.random()*songarr.length);
-	while(rantitle.indexOf(songarr[ran]) > -1)
-	{
-		ran = Math.floor(Math.random()*songarr.length);
-	}
-	rantitle[i] = songarr[ran];
+    var ran = Math.floor(Math.random()*songarr.length);
+    while(answerChoices.indexOf(songarr[ran]) > -1 || songnum == ran)
+    {
+	ran = Math.floor(Math.random()*songarr.length);
+    }
+    answerChoices[i] = songarr[ran];
 }
 
-
+//randomly put the correct answer in the answer choices
 var correctSongNum = Math.floor(Math.random()*4 + 1);
-rantitle[correctSongNum] = songname;
-
+answerChoices[correctSongNum-1] = songname;
 
 window.onload = function()
 {
-	for(i = 1; i <= 4; i ++)
-	{
-		if(i == correctSongNum)
-		{
-			document.getElementById("div"+i).style.color = 'black'
-		}
-		document.getElementById("div"+i).innerHTML = rantitle[i-1];
-	}
+    document.getElementById(songname).play();
+
+    for(i = 1; i <= 4; i ++)
+    {
+	//Set the text for each div		
+	document.getElementById("div"+i).innerHTML = answerChoices[i-1];
+    }
+
+
+
 }
 
-document.getElementById(songname).play();
+function showCorrectAnswer(divClicked)
+{
+    document.getElementById("div"+divClicked).style.color = 'red';
+    document.getElementById("div"+correctSongNum).style.color = 'green';
+}
