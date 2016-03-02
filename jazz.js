09f414f5e38588bebfc6dbb973deea9bf5892a07
Dallas Kelle <dkelle@wireless-10-146-125-144.public.utexas.edu>
2016-03-02 12:05:30 -0600

    One of the four divs now contains the correct Song Name

--- jazz.js
+++ jazz.js
@@ -1,7 +1,6 @@
 var songarr = ["three_to_get_ready", "walking_shoes", "out_of_nowhere", "night_at_the_turntable", "but_not_for_me", "all_the_things_you_are"];
 
 //Populate the array of songs, and pick one of them randomly
-//var songarr = ["three_to_get_ready", "out_of_nowhere" ];
 var songnum = Math.floor(Math.random()*songarr.length);
 var songname = songarr[songnum];
 //Create an array full of random titles
@@ -17,10 +16,18 @@ for(var i = 0; i < 4; i ++)
 }
 
 
+var correctSongNum = Math.floor(Math.random()*4 + 1);
+rantitle[correctSongNum] = songname;
+
+
 window.onload = function()
 {
 	for(i = 1; i <= 4; i ++)
 	{
+		if(i == correctSongNum)
+		{
+			document.getElementById("div"+i).style.color = 'black'
+		}
 		document.getElementById("div"+i).innerHTML = rantitle[i-1];
 	}
 }
