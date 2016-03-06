e5a91d5a3a491c1050d52c0625cae27d9b06be46
Dallas Kelle <dkelle@Dallass-MacBook-Pro.local>
2016-03-06 11:57:26 -0600

    Distributions will automatically change so we ask questions the user is getting wrong. No longer able to click mutliple answers

--- jazz/jazz.js
+++ jazz/jazz.js
@@ -1,20 +1,24 @@
-
 //populate the array of song titles
-
-var songs = ["Ko_Ko", "But_Not_For_Me", "Moon_Dreams", "Nights_At_The_Turn_Table", "O_Grande_Amor", "Black_And_Tan_Fantasy", "Jersey_Bounce", "Madness_In_Great_Ones"]
-var artists = ["Charlie_Parker", "Chet_Baker", "Duke_Ellington", "Gerry_Mulligan", "Stan_Getz", "Duke_Ellington", "Benny_Goodman", "Duke_Ellington"]
-var years = ["1945", "1996", "1949", "1953", "1963", "1930", "1942", "1957"]
-var albums = ["Null", "Blue_Note_Blend_2", "Birth_Of_The_Cool", "Nights_At_The_Turn_Table", "Getz/Gilberto", "Null", "Null", "Such_Sweet_Thunder" ]
+var songs = ["Ko_Ko", "Moon_Dreams", "Nights_At_The_Turn_Table", "O_Grande_Amor", "Black_And_Tan_Fantasy", "Jersey_Bounce", "Madness_In_Great_Ones"]
+var artists = ["Charlie_Parker", "Duke_Ellington", "Gerry_Mulligan", "Stan_Getz", "Duke_Ellington", "Benny_Goodman", "Duke_Ellington"]
+var years = ["1945", "1949", "1953", "1963", "1930", "1942", "1957"]
+var albums = ["Null", "Birth_Of_The_Cool", "Nights_At_The_Turn_Table", "Getz/Gilberto", "Null", "Null", "Such_Sweet_Thunder" ]
 
 //Use this array to determine which question we should ask. Our choices are which song, which artist, which year, and which almub is this song from
-var qOneDistribution = [songs, songs, songs, songs, years, albums, artists, years, albums];
-var qTwoDistribution = [songs, artists, artists, artists, years, albums];
+//var qOneDistribution = [songs, songs, songs, songs, years, albums, artists, years, albums];
+//var qTwoDistribution = [songs, artists, artists, artists, years, albums];
+var qOneDistribution = [songs, songs, songs, songs, years, artists, years,];
+var qTwoDistribution = [songs, artists, artists, artists, years];
 
 var answerChoices = new Array();
 var correctButtonNum;
 var answerIndex;
 var answer;
 var currentSongName;
+var answerSrc;
+var answerSrcTwo;
+var twoQuestions;
+var answerDisplayed;
 
 window.onload = function()
 {
@@ -23,7 +27,7 @@ window.onload = function()
 
 function loadQuestion()
 {
-    var answerSrc = qOneDistribution[Math.floor(Math.random()*qOneDistribution.length)];
+    answerSrc = qOneDistribution[Math.floor(Math.random()*qOneDistribution.length)];
     answerIndex = Math.floor(Math.random()*answerSrc.length);
     //Make sure we aren't asking a quesiton that doesn't have an answer
     while(answerSrc[answerIndex] === "Null")
@@ -34,8 +38,8 @@ function loadQuestion()
     currentSongName = songs[answerIndex];
 
     //35% chance of asking 2 questions
-    var twoQuestions = (Math.floor(Math.random()*100 + 1)) > 35;
-    var answerSrcTwo = qTwoDistribution[Math.floor(Math.random()*qTwoDistribution.length)];
+    twoQuestions = (Math.floor(Math.random()*100 + 1)) > 35;
+    answerSrcTwo = qTwoDistribution[Math.floor(Math.random()*qTwoDistribution.length)];
     //Make sure we aren't asking the same question twice
     while(answerSrc == answerSrcTwo || answerSrcTwo[answerIndex] === "Null")
     {
@@ -89,6 +93,7 @@ function loadQuestion()
     var song = document.getElementById(currentSongName);
     //song.currentTime = Math.floor(Math.random()*(song.duration - song.duration/25));
     song.play();
+    answerDisplayed = false;
 }
 
 function startNewQuestion()
@@ -108,8 +113,49 @@ function startNewQuestion()
 
 function showCorrectAnswer(divClicked)
 {
+    //Check if the correct answer is already being shown. If so, Do nothing
+    if(answerDisplayed)
+    {
+	return;
+    }
+
     //High light the correct answer
     document.getElementById("div"+divClicked).style.background = '#ff6666';
     document.getElementById("div"+correctButtonNum).style.background = '#9ad378';
 
+    alterDistribution(divClicked == correctButtonNum);
+    answerDisplayed = true;
+}
+
+function alterDistribution(answeredCorrectly)
+{
+    //Alter the distributions, so we are less likely to keep asking questions the user is getting right
+    if(answeredCorrectly) 
+    {
+	//User was correct, so take one answerSrc out of distribution, unless it's the only one left
+	var occurences = countOccurences(qOneDistribution, answerSrc);
+	console.log("O: "+ occurences);
+	if(occurences > 1)
+	{
+	    qOneDistribution.splice(qOneDistribution.indexOf(answerSrc), 1);
+	}
+    }
+    else
+    {
+	//The user got the question wrong. Add answerSrc to the distribution so we ask it more often
+	qOneDistribution.push(answerSrc);
+    }
+}
+
+function countOccurences(arr, elem)
+{
+    var count = 0;
+    for(var i = 0; i < arr.length; i ++)
+    {
+	if(arr[i] == elem)
+	{
+	    count ++;
+	}
+    }
+    return count;
 }
